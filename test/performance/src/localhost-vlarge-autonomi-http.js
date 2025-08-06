99f9aad0032abc9354aa7ec776466a0abec4a302
Paul Green <traktion0@gmail.com>
2025-08-07 00:05:54 +0100

    - Update port number for performance tests

--- test/performance/src/localhost-vlarge-autonomi-http.js
+++ test/performance/src/localhost-vlarge-autonomi-http.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
 
 export default function () {
-  http.get('http://localhost:8080/e7bb1b87c1f0e07cdb76ba5e82a425a8da712940c2d3553aa6791494e92aa54d/ubuntu-16.04.6-desktop-i386.iso', { timeout: '600s' });
+  http.get('http://localhost:18888/e7bb1b87c1f0e07cdb76ba5e82a425a8da712940c2d3553aa6791494e92aa54d/ubuntu-16.04.6-desktop-i386.iso', { timeout: '600s' });
 }

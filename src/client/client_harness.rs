c203b29d1818c8458417f5ae950e696efe21d00d
Paul Green <traktion0@gmail.com>
2025-10-15 20:46:19 +0100

    - Bump version of autonomi and chunk-streamer libraries - Bump of supporting libraries

--- src/client/client_harness.rs
+++ src/client/client_harness.rs
@@ -1,6 +1,6 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 use ant_evm::EvmNetwork;
-use autonomi::{BootstrapCacheConfig, Client, ClientConfig, ClientOperatingStrategy, InitialPeersConfig};
+use autonomi::{BootstrapConfig, Client, ClientConfig, ClientOperatingStrategy};
 use log::{debug, info, warn};
 use crate::config::anttp_config::AntTpConfig;
 
@@ -15,7 +15,7 @@ pub struct ClientHarness {
 impl ClientHarness {
     pub fn new(evm_network: EvmNetwork, ant_tp_config: AntTpConfig) -> Self {
         let last_accessed_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-        ClientHarness { evm_network, ant_tp_config, maybe_client: None, last_accessed_time: last_accessed_time }
+        ClientHarness { evm_network, ant_tp_config, maybe_client: None, last_accessed_time }
     }
 
     pub async fn get_client(&mut self) -> Option<Client> {
@@ -40,29 +40,16 @@ impl ClientHarness {
     }
 
     async fn init_client(&self) -> Option<Client> {
-        let bootstrap_cache_config = Some(BootstrapCacheConfig::new(false).unwrap());
-
-        let initial_peers_config = if self.ant_tp_config.peers.clone().is_empty() {
-            InitialPeersConfig::default()
-        } else {
-            InitialPeersConfig {
-                first: false,
-                addrs: self.ant_tp_config.peers.clone(),
-                network_contacts_url: vec![],
-                local: true,
-                ignore_cache: false,
-                bootstrap_cache_dir: Some(bootstrap_cache_config.clone().unwrap().cache_dir),
-            }
-        };
+        let bootstrap_config = BootstrapConfig::new(false)
+            .with_initial_peers(self.ant_tp_config.peers.clone());
 
         let mut strategy = ClientOperatingStrategy::default();
         strategy.chunk_cache_enabled = false; // disable cache to avoid double-caching
 
         match Client::init_with_config(ClientConfig {
-            bootstrap_cache_config: bootstrap_cache_config.clone(),
-            init_peers_config: initial_peers_config,
+            bootstrap_config,
             evm_network: self.evm_network.clone(),
-            strategy: strategy,
+            strategy,
             network_id: Some(1),
         }).await {
             Ok(client) => {

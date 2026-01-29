b33f190c9f17e3e45e8058569bfd9cbd0319efaf
Paul Green <traktion0@gmail.com>
2026-01-29 17:13:54 +0000

    Integrate proto compiler into native Rust build

--- build.rs
+++ build.rs
@@ -1,15 +1,33 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/pointer.proto")?;
-    tonic_build::compile_protos("proto/register.proto")?;
-    tonic_build::compile_protos("proto/chunk.proto")?;
-    tonic_build::compile_protos("proto/graph.proto")?;
-    tonic_build::compile_protos("proto/command.proto")?;
-    tonic_build::compile_protos("proto/pnr.proto")?;
-    tonic_build::compile_protos("proto/public_data.proto")?;
-    tonic_build::compile_protos("proto/public_archive.proto")?;
-    tonic_build::compile_protos("proto/tarchive.proto")?;
-    tonic_build::compile_protos("proto/private_scratchpad.proto")?;
-    tonic_build::compile_protos("proto/public_scratchpad.proto")?;
-    tonic_build::compile_protos("proto/scratchpad.proto")?;
+    println!("cargo::rustc-check-cfg=cfg(grpc_disabled)");
+
+    #[cfg(feature = "grpc")]
+    {
+        use std::process::Command;
+        let protoc_exists = Command::new("protoc").arg("--version").output().is_ok();
+
+        if protoc_exists {
+            tonic_build::compile_protos("proto/pointer.proto")?;
+            tonic_build::compile_protos("proto/register.proto")?;
+            tonic_build::compile_protos("proto/chunk.proto")?;
+            tonic_build::compile_protos("proto/graph.proto")?;
+            tonic_build::compile_protos("proto/command.proto")?;
+            tonic_build::compile_protos("proto/pnr.proto")?;
+            tonic_build::compile_protos("proto/public_data.proto")?;
+            tonic_build::compile_protos("proto/public_archive.proto")?;
+            tonic_build::compile_protos("proto/tarchive.proto")?;
+            tonic_build::compile_protos("proto/private_scratchpad.proto")?;
+            tonic_build::compile_protos("proto/public_scratchpad.proto")?;
+            tonic_build::compile_protos("proto/scratchpad.proto")?;
+        } else {
+            println!("cargo:warning=protoc not found, disabling gRPC support");
+            println!("cargo:rustc-cfg=grpc_disabled");
+        }
+    }
+    #[cfg(not(feature = "grpc"))]
+    {
+        println!("cargo:rustc-cfg=grpc_disabled");
+    }
+
     Ok(())
 }

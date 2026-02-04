9f502a5394a6311e09565ca9e0915c1d649c02e0
Paul Green <traktion0@gmail.com>
2026-02-04 17:04:44 +0000

    - Removed stale/commented code

--- src/client/caching_client.rs
+++ src/client/caching_client.rs
@@ -49,26 +49,7 @@ mock! {
         fn schedule(&self) -> Option<Schedule>;
         async fn handle(&mut self);
     }
-    /*#[async_trait]
-    impl ChunkGetter for CachingClient {
-        async fn chunk_get(&self, address: &ChunkAddress) -> Result<Chunk, autonomi::client::GetError>;
-    }*/
-}
-
-/*#[async_trait]
-impl ChunkGetter for CachingClient {
-    async fn chunk_get(&self, address: &ChunkAddress) -> Result<Chunk, autonomi::client::GetError> {
-        self.chunk_get(address).await
-    }
-}*/
-
-/*#[async_trait]
-impl ChunkGetter for CachingClient {
-    async fn chunk_get(&self, address: &ChunkAddress) -> Result<Chunk, autonomi::client::GetError> {
-        let chunk_caching_client = crate::client::ChunkCachingClient::new(self.clone());
-        chunk_caching_client.chunk_get(address).await
-    }
-}*/
+ }
 
 pub const ARCHIVE_TAR_IDX_BYTES: &[u8] = "\0archive.tar.idx\0".as_bytes();
 
@@ -113,96 +94,6 @@ impl CachingClient {
             fs::create_dir_all(cache_dir.clone()).unwrap_or_default()
         }
     }
-
-    /*pub async fn download_stream(
-        &self,
-        addr: &DataAddress,
-        range_from: i64,
-        range_to: i64,
-    ) -> Result<Bytes, ChunkError> {
-        // todo: combine with file_service code
-        match self.chunk_get(&ChunkAddress::new(*addr.xorname())).await {
-            Ok(data_map_chunk) => {
-                let chunk_streamer = ChunkStreamer::new(addr.to_hex(), data_map_chunk.value, self.clone(), self.ant_tp_config.download_threads);
-
-                // only retrieve the size when it is needed
-                let length = if range_from < 0 || range_to <= 0 {
-                    u64::try_from(chunk_streamer.get_stream_size().await).ok()
-                } else {
-                    None
-                };
-
-                let (derived_range_from, derived_range_to) = self.get_derived_ranges(range_from, range_to, length);
-
-                let mut chunk_receiver: chunk_streamer::chunk_receiver::ChunkReceiver = match chunk_streamer.open(derived_range_from, derived_range_to).await {
-                    Ok(chunk_receiver) => chunk_receiver,
-                    Err(e) => return Err(ChunkError::GetStreamError(GetStreamError::BadReceiver(format!("failed to open chunk stream: {}", e)))),
-                };
-
-                debug!("streaming from addr [{}], range_from: [{}], range_to: [{}], derived_range_from: [{}], derived_range_to: [{}]",
-                    addr, range_from, range_to, derived_range_from, derived_range_to);
-                let mut buf = BytesMut::with_capacity(usize::try_from(derived_range_to - derived_range_from).expect("Failed to convert range from u64 to usize"));
-                let mut has_data = true;
-                while has_data {
-                    match chunk_receiver.next().await {
-                        Some(item) => match item {
-                            Ok(bytes) => buf.put(bytes),
-                            Err(e) => {
-                                error!("Error downloading stream from data address [{}] with range [{} - {}]: {}", addr.to_hex(), derived_range_from, derived_range_to, e);
-                                has_data = false
-                            },
-                        },
-                        None => has_data = false
-                    };
-                }
-                Ok(buf.freeze())
-            }
-            Err(e) => Err(e)
-        }
-    }
-
-    pub fn get_derived_ranges(&self, range_from: i64, range_to: i64, length: Option<u64>) -> (u64, u64) {
-        match length {
-            Some(length) => {
-                let derived_range_from: u64 = if range_from < 0 {
-                    let from = u64::try_from(range_from.abs()).unwrap();
-                    if from < length {
-                        length.saturating_sub(1).saturating_sub(from)
-                    } else {
-                        0 // start from the beginning
-                    }
-                } else {
-                    let from = u64::try_from(range_from).unwrap();
-                    if from > length.saturating_sub(1) {
-                        length.saturating_sub(1)
-                    } else {
-                        from
-                    }
-                };
-                let derived_range_to: u64 = if range_to <= 0 {
-                    let to = u64::try_from(range_to.abs()).unwrap();
-                    if to < length {
-                        length.saturating_sub(1).saturating_sub(to)
-                    } else {
-                        length.saturating_sub(1)
-                    }
-                } else {
-                    let to = u64::try_from(range_to).unwrap();
-                    if to > length.saturating_sub(1) {
-                        length.saturating_sub(1)
-                    } else {
-                        to
-                    }
-                };
-                (derived_range_from, derived_range_to)
-            },
-            None => {
-                let derived_range_from = u64::try_from(range_from.abs()).unwrap();
-                let derived_range_to = u64::try_from(range_to.abs()).unwrap();
-                (derived_range_from, derived_range_to)
-            }
-        }
-    }*/
 
     pub async fn send_create_command(&self, command: Box<dyn Command>) -> Result<(), CreateError> {
         Ok(self.command_executor.send(command).await?)
@@ -289,51 +180,6 @@ mod tests {
 
         (client, rx)
     }
-
-    /*#[tokio::test]
-    async fn test_get_derived_ranges_with_length() {
-        let (client, _) = create_mock_caching_client().await;
-        let length = Some(100u64);
-
-        // Positive ranges within bounds
-        assert_eq!(client.get_derived_ranges(10, 50, length), (10, 50));
-
-        // Positive range_to exceeding length
-        assert_eq!(client.get_derived_ranges(10, 150, length), (10, 99));
-
-        // Positive range_from exceeding length
-        assert_eq!(client.get_derived_ranges(150, 200, length), (99, 99));
-
-        // Negative range_from (from the end)
-        // range_from = -10 means last 10 bytes: from 89 to 99 (length 100)
-        assert_eq!(client.get_derived_ranges(-10, 100, length), (89, 99));
-
-        // Negative range_to (excluding from the end)
-        // range_to = -5: length - 1 - 5 = 94.
-        assert_eq!(client.get_derived_ranges(0, -5, length), (0, 94));
-
-        // Both negative
-        // range_from = -10 (89), range_to = -5 (94)
-        assert_eq!(client.get_derived_ranges(-10, -5, length), (89, 94));
-    }
-
-    #[tokio::test]
-    async fn test_get_derived_ranges_without_length() {
-        let (client, _) = create_mock_caching_client().await;
-
-        // Should just return absolute values
-        assert_eq!(client.get_derived_ranges(10, 50, None), (10, 50));
-        assert_eq!(client.get_derived_ranges(-10, -50, None), (10, 50));
-    }
-
-    #[tokio::test]
-    async fn test_get_derived_ranges_zero_length() {
-        let (client, _) = create_mock_caching_client().await;
-        let length = Some(0u64);
-
-        assert_eq!(client.get_derived_ranges(0, 10, length), (0, 0));
-        assert_eq!(client.get_derived_ranges(-1, -1, length), (0, 0));
-    }*/
 
     #[tokio::test]
     async fn test_new_creates_cache_dir() {
